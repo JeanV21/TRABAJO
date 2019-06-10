95cc683dad6a8e3eb24cae51de0000677ee318eb
JeanV21 <50106949+JeanV21@users.noreply.github.com>
2019-06-10 12:25:50 -0500

    archivo

---
+++ js/accion.js
@@ -0,0 +1,5 @@
+var texto =document.getElementById('t1');
+var texto2 =document.getElementById('t2');
+
+texto.innerHTML="Este es mi proyecto de html el cual insertamos<br/> un texto por medio de javascript.<br/><br/> utilizando<br/> otras extenciones  como css para el estilo y html al<br/> finalizar este proyecto realizaremos otro el cual<br/> tiene mas funcionalidades esperenos pronto GRACIAS:D";
+texto2.innerHTML="Este es mi proyecto de html el cual insertamos<br/> un texto por medio de javascript.<br/><br/> utilizando<br/> otras extenciones  como css para el estilo y html al<br/> finalizar este proyecto realizaremos otro el cual<br/> tiene mas funcionalidades esperenos pronto GRACIAS:D";
